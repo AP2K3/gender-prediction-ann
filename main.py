a9669dcbf298fd6398f2c5ff5023b2c1c7e5f358
amaarirare <arish2k3@gmail.com>
2023-05-19 15:44:11 +0530

    add mse_loss function to calculate model loss

--- main.py
+++ main.py
@@ -18,3 +18,11 @@ Output:
 person_weights = [8, -19, -11, 21] 
 person_weights = [2, -5, -3, 7] 
 '''
+
+def mse_loss(y_true, y_pred):
+    return ((y_true, y_pred) ** 2).mean()
+
+y_true = np.array([1, 0, 0, 1])
+y_pred = np.array([0, 0, 0, 0])
+
+print(mse_loss(y_true, y_pred))
