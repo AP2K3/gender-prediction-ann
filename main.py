5c70780923c1e63beb1bd1395e96cc3c1e2e8917
amaarirare <arish2k3@gmail.com>
2023-05-19 15:08:04 +0530

    add comment for mean minus

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import numpy as np
+
+'''
+Base data:
+person_weights = [133, 160, 152, 120]
+person_heights = [65, 72, 70, 60]
+person_gender = [1, 0, 0, 1]
+
+After shifting by the mean:
+person_weights = [(141 - 133), (141 - 160), (141 - 152), (141 - 120)] 
+person_weights = [(67 - 65), (67 - 72), (67 - 70), (67 - 60)] 
+
+Output:
+person_weights = [8, -19, -11, 21] 
+person_weights = [2, -5, -3, 7] 
+'''
