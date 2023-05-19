fix comma to minus sign

main.py
@@ -20,7 +20,7 @@ person_weights = [2, -5, -3, 7]
 '''
 
 def mse_loss(y_true, y_pred):
-    return ((y_true, y_pred) ** 2).mean()
+    return ((y_true - y_pred) ** 2).mean()
 
 y_true = np.array([1, 0, 0, 1])
 y_pred = np.array([0, 0, 0, 0])